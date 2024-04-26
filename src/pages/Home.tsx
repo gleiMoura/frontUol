03a986f3c6f82dc4ae8291bd30244e8a376175b4
gleiMoura <gle.bar.mou@gmail.com>
2024-04-26 15:41:08 -0300

    ✨ feat: create a new page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import "./reset.css";
+
+import { HeaderComponent } from '../components/Header';
+import { MessagesComponent } from '../components/Messages';
+import { FooterComponent } from '../components/Footer';
+
+export const Home: React.FC = () => {
+    return (
+        <MyHome>
+            <HeaderComponent />
+            <MessagesComponent />
+            <FooterComponent />
+        </MyHome>
+    )
+};
+
+const MyHome = styled.main`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+`
