047d79706c7513c906696ffdc5c882f27b1f69c4
gleiMoura <gle.bar.mou@gmail.com>
2024-04-30 22:31:29 -0300

    ✨ Create message hook to use service message service to get messages

---
+++ src/hooks/useFetchMessages.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { messageType } from "../interfaces/messageInterface";
+import { fetchMessages } from "../services/messageService";
+import { AxiosResponse } from "axios";
+
+export const useFetchUsers = () => {
+    const [messages, setMessages] = useState<messageType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response: AxiosResponse = await fetchMessages();
+                setMessages(response.data);
+                setError(null);
+            } catch (e) {
+                setError('Erro ao carregar dados!');
+                setMessages([]);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { messages, isLoading, error }
+}
