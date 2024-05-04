f01388dc8dff529b2658f0dbe3219610d191dc1c
gleiMoura <gle.bar.mou@gmail.com>
2024-05-03 23:45:14 -0300

    ✨ feat: create an App component with BrowserRouter and routes

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import { useNameFromUser } from './contexts/UserContext';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { InsertNamePage } from './pages/InsertNamePage';
+import { UserProvider } from './contexts/UserContext';
+
+export const App: FC = () => {
+
+    return (
+        <BrowserRouter>
+            <UserProvider>
+                <Routes>
+                    <Route path='/' element={<InsertNamePage />} />
+                    <Route path='/home' element={<Home />} />
+                </Routes>
+            </UserProvider>
+        </BrowserRouter>
+    )
+}
