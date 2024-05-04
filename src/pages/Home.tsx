e86b0d542552e00e7c635c26ef988a3c7ede2513
gleiMoura <gle.bar.mou@gmail.com>
2024-05-04 18:51:34 -0300

    ✨ feat: add a navigate to root by error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,21 @@ import { MessagesComponent } from '../components/Messages';
 import { FooterComponent } from '../components/Footer';
 import { useNavigate } from 'react-router-dom';
 import { useNameFromUser } from '../contexts/UserContext';
+import { useCreateName } from '../hooks/useCreateName';
+import { useErrorMessage } from '../contexts/ErrorContext';
 
 export const Home: React.FC = () => {
     const navigate = useNavigate();
+    const { setErrorText } = useErrorMessage();
     const { name } = useNameFromUser();
+    const { loadingName, errorName } = useCreateName(name);
 
     useEffect(() => {
-        !name && navigate("/");
-    }, [name])
+        if (errorName && !loadingName) {
+            setErrorText(errorName);
+            setTimeout(() => navigate("/"), 3000);
+        }
+    }, [errorName, loadingName])
 
     return (
         <MyHome>
