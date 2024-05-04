4d3a0c4d8b762f5f2871eaa66557d04c1997aacb
gleiMoura <gle.bar.mou@gmail.com>
2024-05-04 18:48:21 -0300

    ✨ feat: add a ErrorProvider at App

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { InsertNamePage } from './pages/InsertNamePage';
 import { UserProvider } from './contexts/UserContext';
+import { ErrorProvider } from './contexts/ErrorContext';
 
 export const App: FC = () => {
 
     return (
         <BrowserRouter>
-            <UserProvider>
-                <Routes>
-                    <Route path='/' element={<InsertNamePage />} />
-                    <Route path='/home' element={<Home />} />
-                </Routes>
-            </UserProvider>
+            <ErrorProvider>
+                <UserProvider>
+                    <Routes>
+                        <Route path='/' element={<InsertNamePage />} />
+                        <Route path='/home' element={<Home />} />
+                    </Routes>
+                </UserProvider>
+            </ErrorProvider>
         </BrowserRouter>
     )
 }
