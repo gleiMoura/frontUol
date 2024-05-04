1644712616a9728ea2e7127dc56b833995736047
gleiMoura <gle.bar.mou@gmail.com>
2024-05-03 23:52:43 -0300

    🐛 fix: if a person try access /home without put a name, it will go to "/" again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import "./reset.css";
 
 import { HeaderComponent } from '../components/Header';
 import { MessagesComponent } from '../components/Messages';
 import { FooterComponent } from '../components/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useNameFromUser } from '../contexts/UserContext';
 
 export const Home: React.FC = () => {
+    const navigate = useNavigate();
+    const { name } = useNameFromUser();
+
+    useEffect(() => {
+        !name && navigate("/");
+    }, [name])
+
     return (
         <MyHome>
             <HeaderComponent />
@@ -20,5 +29,4 @@ const MyHome = styled.main`
     height: 100vh;
     display: flex;
     flex-direction: column;
-    background-color: black;
 `
