efcea133512073d999517b2c8ecff2cb5b84b70b
gleiMoura <gle.bar.mou@gmail.com>
2024-05-05 21:54:46 -0300

    🐛 fix: create participant is responsability from Insert Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,11 @@
-import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import "./reset.css";
 
 import { HeaderComponent } from '../components/Header';
 import { MessagesComponent } from '../components/Messages';
 import { FooterComponent } from '../components/Footer';
-import { useNavigate } from 'react-router-dom';
-import { useNameFromUser } from '../contexts/UserContext';
-import { useCreateName } from '../hooks/useCreateName';
-import { useErrorMessage } from '../contexts/ErrorContext';
 
 export const Home: React.FC = () => {
-    const navigate = useNavigate();
-    const { setErrorText } = useErrorMessage();
-    const { name } = useNameFromUser();
-    const { loadingName, errorName } = useCreateName(name);
-
-    useEffect(() => {
-        if (errorName && !loadingName) {
-            setErrorText(errorName);
-            setTimeout(() => navigate("/"), 3000);
-        }
-    }, [errorName, loadingName])
-
     return (
         <MyHome>
             <HeaderComponent />
