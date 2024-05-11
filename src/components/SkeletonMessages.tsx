3d41f9ded43f300a4533e9a122f1d0b92ca2bc76
gleiMoura <gle.bar.mou@gmail.com>
2024-05-11 18:24:28 -0300

    🐛 fix: skeleton component can be the same way

--- src/components/SkeletonMessages.tsx
+++ src/components/SkeletonMessages.tsx
@@ -1,14 +1,22 @@
 import { Skeleton } from "@mui/material";
 import { FC } from "react";
+import styled from "styled-components";
 
 export const SkeletonMessages: FC = () => {
     return (
         <>
             {
                 Array.from({ length: 15 }, (_, index) => (
-                    <Skeleton style={{marginTop: '5px'}} variant="rectangular" width={'100%'} height={50} key={index} />
+                    <SkeletonBox>
+                        <Skeleton style={{ marginTop: '5px' }} variant="rectangular" width={'100%'} height={50} key={index} />
+                    </SkeletonBox>
                 ))
             }
         </>
     )
 };
+
+const SkeletonBox = styled.div`
+    width: 100%;
+    flex: none;
+`
