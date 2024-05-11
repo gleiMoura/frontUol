02da9f86f7dfa56763269936256fc5f51aff5c23
gleiMoura <gle.bar.mou@gmail.com>
2024-05-11 16:39:51 -0300

    💄 style: change the message style to be big when exist more text

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -6,13 +6,16 @@ export const Message: FC<messageType> = ({ time, text, from, to, type }) => {
     return (
         <MessageComponent type={type}>
             <p>
-                <span className="time">{time}</span> {` `}
+                <span className="time">{time}</span>
+            </p>
+            <p>
                 <span className="from_or_to">{from}</span> {` `}
                 {type === "status" ? "" :
                     <>
                         <span className="message"> para </span>
                         <span className="from_or_to">{to}</span> {`: `}
-                    </>}
+                    </>
+                }
                 <span className="message">{text}</span>
             </p>
         </MessageComponent>
@@ -22,7 +25,7 @@ export const Message: FC<messageType> = ({ time, text, from, to, type }) => {
 //styled-components
 const MessageComponent = styled.div<messageType>`
     width: 100%;
-    height: 50px;
+    min-height: 50px;
     margin-top: 10px;
     font-size: 2,5vh;
     background-color: ${props => {
@@ -38,19 +41,21 @@ const MessageComponent = styled.div<messageType>`
     box-sizing: border-box;  
 
     .time{
-    color: #AAA;
-}
+        color: #AAA;
+        margin-right: 15px;
+    }
+
+
+    .from_or_to {
+        color: #000;
+        font-family: 'Roboto';
+        font-weight: 600;
+    }
 
     .message {
     color: #000;
     font-family: 'Roboto';
     font-weight: 400;
-}
-
-    .from_or_to {
-    color: #000;
-    font-family: 'Roboto';
-    font-weight: 600;
-}
+    }
 `;
 
