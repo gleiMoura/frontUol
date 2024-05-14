e512ede9fb1b570e56e45648e0d09151661ee4fe
gleiMoura <gle.bar.mou@gmail.com>
2024-05-14 11:32:48 -0300

    ✨ feat: create a generic skeleton to every component

---
+++ src/components/GenericSkeleton.tsx
@@ -0,0 +1,28 @@
+import { Skeleton } from "@mui/material";
+import { FC } from "react";
+import styled from "styled-components";
+
+interface GenericSkeletonProp {
+    number: number;
+    marginTop: string;
+    height: number;
+}
+
+export const GenericSkeleton: FC<GenericSkeletonProp> = ({ number, marginTop, height }) => {
+    return (
+        <>
+            {
+                Array.from({ length: number }, (_, index) => (
+                    <SkeletonBox>
+                        <Skeleton style={{ marginTop: `${marginTop}` }} variant="rectangular" width={'100%'} height={height} key={index} />
+                    </SkeletonBox>
+                ))
+            }
+        </>
+    )
+};
+
+const SkeletonBox = styled.div`
+    width: 100%;
+    flex: none;
+`
