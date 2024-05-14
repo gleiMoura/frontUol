e5b3f2f05de0e70f4e5099e517cc38c220b2fa79
gleiMoura <gle.bar.mou@gmail.com>
2024-05-14 14:30:32 -0300

    🐛 fix: use a state called "created" to know that participant was created

--- src/hooks/useCreateName.tsx
+++ src/hooks/useCreateName.tsx
@@ -1,15 +1,20 @@
 import { useCallback, useState } from "react";
 import { createUser } from "../services/participantService";
 import { useErrorMessage } from "../contexts/ErrorContext";
+import useLocalStorage from "./useLocalSorage";
 
 export const useCreateName = () => {
     const [loadingName, setLoadingName] = useState(false);
+    const [created, setCreated] = useState("");
     const { setErrorText } = useErrorMessage();
+    const { setDataInLocalStorage } = useLocalStorage("name");
 
-    const create = useCallback(async (name: string | null) => {
+    const create = useCallback(async (name: string) => {
         try {
             setLoadingName(true)
             await createUser(name);
+            setCreated("created");
+            setDataInLocalStorage("name", name)
         } catch (e: any) {
             console.log("erro", e)
             setErrorText(e?.response?.data[0]?.message || e?.response?.data || 'Erro inesperado. Tente novamente mais tarde!');
@@ -18,5 +23,5 @@ export const useCreateName = () => {
         }
     }, []);
 
-    return { loadingName, create }
+    return { loadingName, create, created, setCreated }
 };
