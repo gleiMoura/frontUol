81cddd5b3f6b4c16e818c69bc6c64143ec5c3eb5
gleiMoura <gle.bar.mou@gmail.com>
2024-05-14 14:27:51 -0300

    ✨ feat: put provider from message context in app

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { Home } from './pages/Home';
 import { InsertNamePage } from './pages/InsertNamePage';
 import { UserProvider } from './contexts/UserContext';
 import { ErrorProvider } from './contexts/ErrorContext';
+import { MessageProvider } from './contexts/messageContext';
 
 export const App: FC = () => {
     return (
         <BrowserRouter>
             <ErrorProvider>
-                <UserProvider>
-                    <Routes>
-                        <Route path='/' element={<InsertNamePage />} />
-                        <Route path='/home' element={<Home />} />
-                    </Routes>
-                </UserProvider>
+                <MessageProvider>
+                    <UserProvider>
+                        <Routes>
+                            <Route path='/' element={<InsertNamePage />} />
+                            <Route path='/home' element={<Home />} />
+                        </Routes>
+                    </UserProvider>
+                </MessageProvider>
             </ErrorProvider>
         </BrowserRouter>
     )
