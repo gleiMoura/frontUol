e12f9b2f7cd8dfc72d41977a66a64478991ef13d
gleiMoura <gle.bar.mou@gmail.com>
2024-05-14 11:34:56 -0300

    ♻️ refactor: change response word to users

--- src/hooks/useFetchParticipants.tsx
+++ src/hooks/useFetchParticipants.tsx
@@ -2,17 +2,19 @@ import { useEffect, useState } from "react";
 import { getParticipants } from "../services/participantService";
 import { userType } from "../interfaces/messageInterface";
 import { AxiosResponse } from "axios";
+import { useErrorMessage } from "../contexts/ErrorContext";
 
 export const useFetchParticipants = () => {
     const [participants, setParticipants] = useState<userType[]>([]);
     const [loadingUsers, setLoadingUsers] = useState(true);
+    const { setErrorText } = useErrorMessage()
 
     const fetchData = async () => {
         try {
-            const response: AxiosResponse = await getParticipants();
-            setParticipants(response.data);
-        } catch (error) {
-            console.log(error)
+            const users: AxiosResponse = await getParticipants();
+            setParticipants(users.data);
+        } catch (e) {
+            setErrorText('Erro ao carregar participantes!')
         } finally {
             setLoadingUsers(false);
         }
