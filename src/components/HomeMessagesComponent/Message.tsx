14ae89fa9312305698d5e9c22047649acc04acaa
gleiMoura <gle.bar.mou@gmail.com>
2024-05-17 01:00:41 -0300

    ✨ feat: create new components to message in a folder

---
+++ src/components/HomeMessagesComponent/Message.tsx
@@ -0,0 +1,62 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { messageType } from "../../interfaces/messageInterface";
+
+export const Message: FC<messageType> = ({ time, text, from, to, type }) => {
+    return (
+        <MessageComponent type={type}>
+            <p>
+                <span className="time">{time}</span>
+            </p>
+            <p>
+                <span className="from_or_to">{from}</span> {` `}
+                {type === "status" ? "" :
+                    <>
+                        <span className="message"> para </span>
+                        <span className="from_or_to">{to}</span> {`: `}
+                    </>
+                }
+                <span className="message">{text}</span>
+            </p>
+        </MessageComponent>
+    )
+};
+
+//styled-components
+const MessageComponent = styled.div<messageType>`
+    width: 100%;
+    min-height: 50px;
+    margin-top: 10px;
+    font-size: 2,5vh;
+    background-color: ${props => {
+        return props.type === "status" ? '#DCDCDC' :
+            props.type === "message" ? '#FFFFFF' :
+                props.type === "private_message" ? '#FFDEDE' : '#fff';
+    }};
+
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 10px;
+    box-sizing: border-box;
+    flex: none;
+    
+    .time{
+        color: #AAA;
+        margin-right: 15px;
+    }
+
+
+    .from_or_to {
+        color: #000;
+        font-family: 'Roboto';
+        font-weight: 600;
+    }
+
+    .message {
+    color: #000;
+    font-family: 'Roboto';
+    font-weight: 400;
+    }
+`;
+
---
+++ src/components/HomeMessagesComponent/Messages.tsx
@@ -0,0 +1,82 @@
+//react
+import styled from "styled-components";
+import { FC, useEffect } from "react";
+//hooks and functions
+import { useErrorMessage } from "../../contexts/ErrorContext";
+import { useNavigate } from "react-router-dom";
+import { useFetchMessages } from "../../hooks/useFetchMessages";
+import useLocalStorage from "../../hooks/useLocalSorage";
+import { updateParcipant } from "../../services/statusService";
+//components
+import { Message } from "./Message";
+import { GenericSkeleton } from "../GenericSkeleton";
+
+export const MessagesComponent: FC = () => {
+    const navigate = useNavigate();
+    const { errorText, setErrorText } = useErrorMessage();
+    const { value: user } = useLocalStorage("name");
+    const { messages, loadingMessages } = useFetchMessages(user);
+
+    useEffect(() => {
+        if (errorText === "Erro ao carregar dados!") {
+            navigate("/");
+        }
+    }, [errorText])
+
+    useEffect(() => {
+        let intervalUpdate = setInterval(async () => {
+            try {
+                await updateParcipant(user);
+            } catch (e) {
+                setErrorText("Não foi possível manter a conexão. Entre com outro nome.");
+                clearInterval(intervalUpdate);
+                navigate("/")
+            }
+        }, 5000);
+
+        return () => {
+            clearInterval(intervalUpdate)
+        }
+    }, [user])
+
+    if (loadingMessages) {
+        return (
+            <MainMessages>
+                <GenericSkeleton number={15} marginTop="5px" height={40} />
+            </MainMessages>
+        )
+    } else {
+        return (
+            <>
+                <MainMessages>
+                    {[...messages].reverse().map((message) => {
+                        return (
+                            <Message
+                                key={message._id}
+                                time={`(${message.time})`}
+                                text={message.text}
+                                from={message.from}
+                                to={message.to}
+                                type={message.type}
+                            />
+                        )
+                    })}
+                </MainMessages>
+            </>
+        )
+    }
+};
+
+//styled-components
+const MainMessages = styled.section`
+    width: 100%;
+    height: 80%;
+    padding: 2vh 0;
+    box-sizing: border-box;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.20) inset;
+    background-color: #E5E5E5;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: flex-start;
+    `;
