999ecd8b27dd81c9f61271b0ff0abe51b61ddf84
gleiMoura <gle.bar.mou@gmail.com>
2024-05-17 00:59:46 -0300

    ✨ feat: create a new footer in a folder

---
+++ src/components/HomeFooterComponent/Footer.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import useLocalStorage from "../../hooks/useLocalSorage";
+import { ChangeEvent, FC } from "react";
+import { LuSend } from "react-icons/lu";
+import { sendMessage } from "../../services/messageService";
+import { useErrorMessage } from "../../contexts/ErrorContext";
+import { useMessageContext } from "../../contexts/messageContext";
+
+export const FooterComponent: FC = () => {
+    const { userMessage, setUserMessage } = useMessageContext();
+    const { value: user }: { value: string } = useLocalStorage("name");
+    const { setErrorText } = useErrorMessage();
+
+    const handleChangeMessage = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setUserMessage(prevState => ({
+            ...prevState,
+            text: event.target.value
+        }))
+    };
+
+    const handleClickMessage = async () => {
+        try {
+            await sendMessage(user, userMessage);
+            setUserMessage(prevState => ({
+                ...prevState,
+                text: ""
+            }))
+        } catch (e) {
+            console.log(e);
+            setErrorText("Erro ao carregar dados!")
+        }
+    }
+
+    return (
+        <>
+            <MainFooter>
+                <TextInput
+                    rows={4}
+                    cols={10}
+                    placeholder="Esvreva aqui..."
+                    onChange={handleChangeMessage}
+                    value={userMessage.text}
+                />
+                <SendButton onClick={handleClickMessage}>
+                    <LuSend size={30} />
+                </SendButton>
+            </MainFooter>
+        </>
+    )
+};
+
+const MainFooter = styled.footer`
+    width: 100%;
+    height: 60px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.20) inset;
+    background-color: white;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const TextInput = styled.textarea`
+    width: 50%;
+    height: 40%;
+    font-size: 1rem;
+    border: none;
+    outline: none;
+    resize: none;
+    padding: 5px
+`;
+
+const SendButton = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+`
